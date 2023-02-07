85e892b3a79a0614f1ccc154a37f4b624a74afdb
Ernesto Vizcaíno <vizcaino.erne@gmail.com>
2023-02-07 17:07:11 -0600

    Secodnd version

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const request = require("request");
 const express = require("express");
 const numeral = require("numeral");
+const escpos = require("escpos");
 const moment = require("moment");
 const sharp = require("sharp");
 const cors = require("cors");
 const fs = require("fs");
 
-const { Printer, Image } = require("@node-escpos/core");
+escpos.USB = require("escpos-usb");
 
-const USB = require("@node-escpos/usb-adapter");
+const device = new escpos.USB();
 
-const device = new USB();
 const app = express();
 
 app.use(cors());
@@ -68,59 +68,66 @@ const resizeImage = (sale) =>
 const printImage = (sale) => {
   const options = { encoding: "UTF8" /* default */ };
 
-  device.open(async (err) => {
-    if (err) {
-      console.log(err);
-    }
-    let printer = new Printer(device, options);
-    const image = await Image.load("resized.png");
+  escpos.Image.load("resized.png", function (image) {
+    device.open(async (err) => {
+      if (err) {
+        console.log(err);
+      }
+      const printer = new escpos.Printer(device, options);
 
-    printer = await printer.image(image, "D24");
-
-    printer
-      .font("a")
-      .align("lt")
-      .text(sale.header[0].business_address)
-      .text(`Tel: ${sale.header[0].business_phone}`)
-      .text(`Fecha: ${moment(sale.header[0].created_at).format("DD/MM/YYYY")}`)
-      .text(`Hora: ${moment(sale.header[0].created_at).format("hh:mm:ss")}`)
-      .text("================================")
-      .style("b")
-      .text("Productos")
-      .text("================================");
-
-    sale.products.forEach((product) => {
       printer
         .font("a")
-        .style("normal")
-        .text(product.name)
-        .text(
-          `${numeral(product.price).format("$0,0.00")} c/u x${
-            product.cantidad
-          } (${numeral(product.total).format("$0,0.00")})`
-        );
+        .align("lt")
+        .image(image)
+        .then(() => {
+          printer
+            .text(sale.header[0].business_address)
+            .text(`Tel: ${sale.header[0].business_phone}`)
+            .text(
+              `Fecha: ${moment(sale.header[0].created_at).format("DD/MM/YYYY")}`
+            )
+            .text(
+              `Hora: ${moment(sale.header[0].created_at).format("hh:mm:ss")}`
+            )
+            .text("================================")
+            .style("b")
+            .text("Productos")
+            .text("================================");
+
+          sale.products.forEach((product) => {
+            printer
+              .font("a")
+              .style("normal")
+              .text(product.name)
+              .text(
+                `${numeral(product.price).format("$0,0.00")} c/u x${
+                  product.cantidad
+                } (${numeral(product.total).format("$0,0.00")})`
+              );
+          });
+
+          printer
+            .text("================================")
+            .align("rt")
+            .text(`Total: ${numeral(sale.header[0].value).format("$0,0.00")}`)
+            .text(
+              `Método de pago: ${
+                ["Efectivo", "Tarjeta", "Transferencia", "Otro", "Deuda"][
+                  sale.header[0].payment_metod - 1
+                ]
+              }`
+            )
+            .feed();
+
+          printer
+            .align("ct")
+            .text("Hecho con Oliver.")
+            .feed()
+            .text("Descarga la app \n y maneja tu negocio en:")
+            .text("https://somosoliver.com");
+
+          printer.cut().close();
+        });
     });
-
-    printer
-      .text("================================")
-      .align("rt")
-      .text(`Total: ${numeral(sale.header[0].value).format("$0,0.00")}`)
-      .text(
-        `Método de pago: ${
-          ["Efectivo", "Tarjeta", "Transferencia", "Otro", "Deuda"][
-            sale.header[0].payment_metod - 1
-          ]
-        }`
-      )
-      .feed();
-
-    printer
-      .align("ct")
-      .text("Hecho con Oliver.")
-      .feed()
-      .text("Descarga la app \n y maneja tu negocio en:")
-      .text("https://somosoliver.com");
-
-    printer.cut().close();
   });
 };
